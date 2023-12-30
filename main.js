74957e266403d90273f4d2df14fd6d34e6152f16
snjy7773 <sanjay1993sanjay1993@gmail.com>
2023-12-30 08:37:21 +0530

    ok ...css pandding

---
+++ main.js
@@ -0,0 +1,30 @@
+var start = document.querySelector("#start");
+var stop = document.querySelector("#stop");
+var h3 = document.querySelector("#h3");
+var li = document.createElement('li');
+var ul = document.querySelector("#ul")
+var  st;
+var count;
+start.addEventListener("click", function(){
+    // alert("chala") 
+    
+    
+     count = 0;
+    st =  setInterval(function() {
+       
+       h3.textContent = count;
+       count++ 
+    } ,500) 
+
+    
+})
+
+stop.addEventListener( "click", function(){
+    clearInterval(st);
+    li = document.createElement('li');
+    ul.appendChild(li);
+    li.textContent = count - 1 ;
+    
+
+}
+)
